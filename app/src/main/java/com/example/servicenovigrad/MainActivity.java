949b690c79a6472ac83bfdf7fda99dd3d099f0aa
Hened Saade <hsaad097@uottawa.ca>
2020-10-15 20:40:37 -0400

    Created interface layout
    Created login, signup and welcome page

--- app/src/main/java/com/example/servicenovigrad/MainActivity.java
+++ app/src/main/java/com/example/servicenovigrad/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.servicenovigrad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.servicenovigrad.ui.login.SignUpPage;
 
 public class MainActivity extends AppCompatActivity {
+    private Button loginButton;
+    private Button signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = (Button) findViewById(R.id.login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWelcomePage();
+            }
+        });
+
+        signupButton = (Button) findViewById(R.id.signup);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignUpPage();
+            }
+        });
+    }
+
+    public void openWelcomePage() {
+        Intent intent = new Intent(this, WelcomePage.class);
+        startActivity(intent);
+    }
+
+    public void openSignUpPage() {
+        Intent intent = new Intent(this, SignUpPage.class);
+        startActivity(intent);
     }
 }
