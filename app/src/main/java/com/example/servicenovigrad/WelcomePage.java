256a29eb13eb7efeb84e0f25fcfa400dbbe4e011
Wilfried SANKARA <wsank045@uottawa.ca>
2020-10-17 17:53:41 -0400

    J'ai travaillé sur la page d'Acceuil mais elle est inachevée

--- app/src/main/java/com/example/servicenovigrad/WelcomePage.java
+++ app/src/main/java/com/example/servicenovigrad/WelcomePage.java
@@ -24,16 +24,9 @@ public class WelcomePage extends AppCompatActivity {
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
+        TextView welcomeMessage= (TextView) findViewById(R.Id.welcome);
+        welcomeMessage.setText("Bienvenue, ")
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
