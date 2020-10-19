a0590a43cf294c8f08260e4b51b880e2068bb315
Wilfried SANKARA <wsank045@uottawa.ca>
2020-10-18 23:49:45 -0400

    Update WelcomePage.java

--- app/src/main/java/com/example/servicenovigrad/WelcomePage.java
+++ app/src/main/java/com/example/servicenovigrad/WelcomePage.java
@@ -10,17 +10,34 @@ import android.text.TextUtils;
 import android.util.Patterns;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WelcomePage extends AppCompatActivity {
     private TextView welcomeMessage;
+    private FirebaseUser user;
+    String prenom;
+    //AccountType type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
         welcomeMessage= (TextView) findViewById(R.id.welcome);
-        welcomeMessage.setText("Bienvenue, ");
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user!=null){
+            prenom=user.getDisplayName();
+        }
+        welcomeMessage.setText("Bienvenue "+ prenom);
 
     }
 }
