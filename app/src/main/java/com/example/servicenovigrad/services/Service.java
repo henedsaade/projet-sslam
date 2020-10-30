ce9577be168340fe2fd1d0f04bb79f8e560cd5da
zachlegros <legroszach3921@gmail.com>
2020-10-30 17:01:13 -0400

    implemented Service class

--- app/src/main/java/com/example/servicenovigrad/services/Service.java
+++ app/src/main/java/com/example/servicenovigrad/services/Service.java
@@ -23,43 +23,13 @@ public class Service {
     private String serviceName;
 
     private String uid;
-    private HashMap<String, Document> documents;
-    private Form form;
 
     // constructor for a service stored in firestore
     public Service(String serviceUid) throws FirebaseFirestoreException {
         this.uid = serviceUid;
-        fetchServiceInformations();
-    }
 
-    public Service(String serviceName, String[] formFields, String[] documentsNames) {
-        this.serviceName = serviceName;
-        this.formFields = formFields;
-        this.documentsNames = documentsNames;
-
-        this.form = new Form(formFields);
-        this.documents =
-    }
-
-    public void initializeForm()
-
-    // Method to save the service blueprint to firebase.
-    public void saveServiceInformations() throws Exception {
-        HashMap<String, Object> dataToSave = new HashMap<String, Object>();
-        dataToSave.put("formFields", formFields);
-        dataToSave.put("documentsNames", documentsNames);
-        dataToSave.put("serviceName", serviceName);
-
-        try {
-            FbWrapper.getInstance().setDocument(firestoreServicesRoute, dataToSave);
-        } catch (FirebaseFirestoreException e) {
-            Log.d(TAG, e.getMessage());
-            throw new Exception("Error while trying to save the service to firestore.");
-        }
-    }
-
-    public Task<DocumentSnapshot> fetchServiceInformations() throws FirebaseFirestoreException {
-        return db.document(firestoreServicesRoute + uid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+        // fetch service blueprint from uid
+        db.document(firestoreServicesRoute + uid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                 if (task.isSuccessful()) {
@@ -82,4 +52,41 @@ public class Service {
         });
     }
 
+    public Service(String serviceName, String[] formFields, String[] documentsNames) throws NullPointerException {
+        if (serviceName == null || formFields == null || documentsNames == null) {
+            throw new NullPointerException();
+        }
+        this.serviceName = serviceName;
+        this.formFields = formFields;
+        this.documentsNames = documentsNames;
+    }
+
+    // Method to save the service blueprint to firebase.
+    public void saveServiceBlueprint() throws Exception {
+        if (formFields != null && documentsNames != null && serviceName != null) {
+            HashMap<String, Object> dataToSave = new HashMap<String, Object>();
+            dataToSave.put("formFields", formFields);
+            dataToSave.put("documentsNames", documentsNames);
+            dataToSave.put("serviceName", serviceName);
+
+            try {
+                FbWrapper.getInstance().setDocument(firestoreServicesRoute, dataToSave);
+            } catch (FirebaseFirestoreException e) {
+                Log.d(TAG, e.getMessage());
+                throw new Exception("Error while trying to save the service to firestore.");
+            }
+        }
+    }
+
+    public String[] getFormFields() {
+        return this.formFields;
+    }
+
+    public String[] getDocumentsNames() {
+        return this.documentsNames;
+    }
+
+    public String getServiceName() {
+        return this.serviceName;
+    }
 }
