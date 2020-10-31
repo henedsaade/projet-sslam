dd84b2e5b13586172ff38d2bc930813f17261a8f
Wsank045 <62038563+Wsank045@users.noreply.github.com>
2020-10-30 23:15:38 -0400

    Modifications sur addservice

---
+++ app/src/main/java/com/example/servicenovigrad/WelcomePage.java
@@ -0,0 +1,43 @@
+package com.example.servicenovigrad;
+
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class WelcomePage extends AppCompatActivity {
+    private TextView welcomeMessage;
+    private String prenom;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_page);
+        welcomeMessage= (TextView) findViewById(R.id.welcome);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user!=null){
+            prenom=user.getDisplayName();
+        }
+        welcomeMessage.setText("Bienvenue "+ prenom);
+
+    }
+}
